c36e399c67acadede0b615385a21d5b88263c848
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-10-03 11:18:34 -0300

    Add files via upload

---
+++ selection_sort_crescente.js
@@ -0,0 +1,17 @@
+/**
+ *@param {number []} arr - o array a ser condenado.
+ *@return {number []} - o array ordenado em ordem crescente.
+*/
+
+function selectionSortAscending(arr){
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++){
+        let minIndex = j;
+    }
+}
+
+if (minIndex !== i){
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+}
